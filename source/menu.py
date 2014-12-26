5b04b9c3b04550ba9461264255dcf8f1a096c9be
Mozzo <mozzo242@gmail.com>
2014-12-26 23:26:55 +0100

    Changed formatting a bit.

--- source/menu.py
+++ source/menu.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 import os, sys, curses, atexit, time
 
 import sys
@@ -7,81 +5,80 @@ reload(sys)
 sys.setdefaultencoding("utf-8")
 
 class cmenu(object):
-    datum = {}
-    ordered = []
-    pos = 0
+	datum = {}
+	ordered = []
+	pos = 0
 
-    def __init__(self, options, title="Menu"):
-        curses.initscr()
-        curses.start_color()
-        curses.use_default_colors()
-        curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
-        curses.curs_set(0)
-        self.screen = curses.initscr()
-        self.screen.keypad(1)
+	def __init__(self, options, title="Menu"):
+		curses.initscr()
+		curses.start_color()
+		curses.use_default_colors()
+		curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
+		curses.curs_set(0)
+		self.screen = curses.initscr()
+		self.screen.keypad(1)
 
-        self.h = curses.color_pair(1)
-        self.n = curses.A_NORMAL
+		self.h = curses.color_pair(1)
+		self.n = curses.A_NORMAL
 
-        for item in options:
-            k, v = item.items()[0]
-            self.datum[k] = v
-            self.ordered.append(k)
+		for item in options:
+			k, v = item.items()[0]
+			self.datum[k] = v
+			self.ordered.append(k)
 
-        self.title = title
+		self.title = title
 
-        atexit.register(self.cleanup)
+		atexit.register(self.cleanup)
 
-    def cleanup(self):
-        curses.doupdate()
-        curses.endwin()
+	def cleanup(self):
+		curses.doupdate()
+		curses.endwin()
 
-    def upKey(self):
-        if self.pos == (len(self.ordered) - 1):
-            self.pos = 0
-        else:
-            self.pos += 1
+	def upKey(self):
+		if self.pos == (len(self.ordered) - 1):
+			self.pos = 0
+		else:
+			self.pos += 1
 
-    def downKey(self):
-        if self.pos <= 0:
-            self.pos = len(self.ordered) - 1
-        else:
-            self.pos -= 1
+	def downKey(self):
+		if self.pos <= 0:
+			self.pos = len(self.ordered) - 1
+		else:
+			self.pos -= 1
 
-    def display(self):
-        screen = self.screen
-        while True:
-            screen.clear()
-            screen.addstr(2, 2, self.title, curses.A_STANDOUT|curses.A_BOLD)
-            screen.addstr(4, 2, "Please select an option...", curses.A_BOLD)
+	def display(self):
+		screen = self.screen
+		while True:
+			screen.clear()
+			screen.addstr(2, 2, self.title, curses.A_STANDOUT|curses.A_BOLD)
+			screen.addstr(4, 2, "Please select an option...", curses.A_BOLD)
 
-            ckey = None
-            func = None
+			ckey = None
+			func = None
 
-            while ckey != ord('\n'):
-                for n in range(0, len(self.ordered)):
-                    optn = self.ordered[n]
+			while ckey != ord('\n'):
+				for n in range(0, len(self.ordered)):
+					optn = self.ordered[n]
 
-                    if n != self.pos:
-                            screen.addstr(6 + n, 4, "%d. %s" % (n, optn), self.n)
-                    else:
-                        screen.addstr(6 + n, 4, "%d. %s" % (n, optn), self.h)
-                screen.refresh()
+					if n != self.pos:
+						screen.addstr(6 + n, 4, "%d. %s" % (n, optn), self.n)
+					else:
+						screen.addstr(6 + n, 4, "%d. %s" % (n, optn), self.h)
+				screen.refresh()
 
-                ckey = screen.getch()
+				ckey = screen.getch()
 
-                if ckey == 258:
-                    self.upKey()
+				if ckey == 258:
+					self.upKey()
+				if ckey == 259:
+					self.downKey()
 
-                if ckey == 259:
-                    self.downKey()
-
-            ckey = 0
-            self.cleanup()
-            if self.pos >= 0 and self.pos < len(self.ordered):
-                self.datum[self.ordered[self.pos]]()
-                self.pos = -1
-            else:
-                curses.flash()
+			ckey = 0
+			self.cleanup()
+			if self.pos >= 0 and self.pos < len(self.ordered):
+				self.datum[self.ordered[self.pos]]()
+				self.pos = -1
+			else:
+				curses.flash()
 
 
